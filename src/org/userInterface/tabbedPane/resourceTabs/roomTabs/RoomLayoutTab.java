2b11568cc94cc23d82df1b82d824f070e1403f3d
QuinnBast <Quinn.Bast@hotmail.com>
2018-05-14 18:30:49 -0600

    *Some work on room editor

--- src/org/userInterface/tabbedPane/resourceTabs/roomTabs/RoomLayoutTab.java
+++ src/org/userInterface/tabbedPane/resourceTabs/roomTabs/RoomLayoutTab.java
@@ -1,9 +1,20 @@
 package org.userInterface.tabbedPane.resourceTabs.roomTabs;
 
+import org.userInterface.tabbedPane.resourceTabs.roomTabs.roomEditor.RoomEditorCanvas;
+
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * Created by Quinn on 5/11/2018.
  */
 public class RoomLayoutTab extends JComponent {
+
+    RoomEditorCanvas roomEditor = new RoomEditorCanvas();
+
+    public RoomLayoutTab(){
+        this.setLayout(new BorderLayout());
+        this.add(roomEditor, BorderLayout.CENTER);
+    }
+
 }
