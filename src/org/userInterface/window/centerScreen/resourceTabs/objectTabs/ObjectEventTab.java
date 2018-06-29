079bf62d6735ebb3b48e53eb955a5bd492789e04
QuinnBast <Quinn.Bast@hotmail.com>
2018-06-29 16:15:42 -0600

    *Implemented basic code box for future development. Will eventually add selector for event types and more text-editor functionality like find and replace, find, etc.

--- src/org/userInterface/window/centerScreen/resourceTabs/objectTabs/ObjectEventTab.java
+++ src/org/userInterface/window/centerScreen/resourceTabs/objectTabs/ObjectEventTab.java
@@ -1,8 +1,13 @@
 package org.userInterface.window.centerScreen.resourceTabs.objectTabs;
 
 import org.developmentEngine.resourceManager.resourceProperties.ResourceProperties;
+import org.fife.ui.rsyntaxtextarea.RSyntaxTextArea;
+import org.fife.ui.rsyntaxtextarea.SyntaxConstants;
+import org.fife.ui.rtextarea.RTextScrollPane;
 import org.userInterface.window.centerScreen.resourceTabs.Tab;
 import org.developmentEngine.resourceManager.Resources.Resource;
+
+import javax.swing.*;
 
 /**
  * Created by Quinn on 5/11/2018.
@@ -10,6 +15,13 @@ import org.developmentEngine.resourceManager.Resources.Resource;
 public class ObjectEventTab extends Tab {
     public ObjectEventTab(Resource r) {
         super(r);
+        JPanel eventPanel = new JPanel();
+        RSyntaxTextArea syntaxTextArea = new RSyntaxTextArea(20, 60);
+        syntaxTextArea.setSyntaxEditingStyle(SyntaxConstants.SYNTAX_STYLE_JAVA);
+        syntaxTextArea.setCodeFoldingEnabled(true);
+        RTextScrollPane scrollPane = new RTextScrollPane(syntaxTextArea);
+        eventPanel.add(scrollPane);
+        this.add(eventPanel);
     }
 
     @Override
